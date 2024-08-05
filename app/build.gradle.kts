8d690d89e3de9243cd6f00ac8b6b116262cfb63a
Baruch Ginzburg <W050459990@gmail.com>
2024-08-05 22:00:15 +0300

    rounded profile img

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
